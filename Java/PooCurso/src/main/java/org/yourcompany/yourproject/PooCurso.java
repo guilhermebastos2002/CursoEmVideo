913750fde14f109537fa1fc7f25ac8a46ab2923e
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2025-05-19 17:02:16 -0300

    Add files via upload

---
+++ Java/PooCurso/src/main/java/org/yourcompany/yourproject/PooCurso.java
@@ -0,0 +1,15 @@
+package org.yourcompany.yourproject;
+
+public class PooCurso {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.maisVolume();
+        c.menosVolume();
+        c.menosVolume();
+        c.menosVolume();
+        c.abrirMenu();
+    }
+}
